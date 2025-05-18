b79ca53c3b986e01df9c0a8586d5fdffb246ce14
JanRibka <62721322+JanRibka@users.noreply.github.com>
2025-05-18 21:35:54 +0200

    Admin layout

--- src/app/admin/(protected)/layout.tsx
+++ src/app/admin/(protected)/layout.tsx
@@ -28,15 +28,15 @@ export default async function ProtectedLayout({ children }: Props) {
 
   return (
     <SetUser user={session.user as AdapterUser}>
-      <>
+      <div className="flex flex-col h-screen">
         <NavBar />
         <SideBar />
-        <div className="ml-0 md:ml-24 xl:ml-40 2xl:ml-44 transition-all duration-200 ease-linear h-full">
-          <div className="pt-[0.938rem] md:pt-[1.875rem] 2xl:pt-10 px-[0.938rem] md:px-[1.875rem] 2xl:px-10 mx-auto bg-pageBackground h-full ">
+        <div className="flex-1 ml-0 md:ml-24 xl:ml-40 2xl:ml-44 transition-all duration-200 ease-linear overflow-auto">
+          <div className="pt-[0.938rem] md:pt-[1.875rem] 2xl:pt-10 px-[0.938rem] md:px-[1.875rem] 2xl:px-10 mx-auto bg-pageBackground h-full overflow-hidden">
             {children}
           </div>
         </div>
-      </>
+      </div>
     </SetUser>
   );
 }

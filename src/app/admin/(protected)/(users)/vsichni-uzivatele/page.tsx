4118f8a542cedc430b09f292794df0a0a9ce7630
JanRibka <62721322+JanRibka@users.noreply.github.com>
2025-05-22 10:23:11 +0200

    Remove Suspense from AllUsersPage

--- src/app/admin/(protected)/(users)/vsichni-uzivatele/page.tsx
+++ src/app/admin/(protected)/(users)/vsichni-uzivatele/page.tsx
@@ -1,14 +1,10 @@
-import { Suspense } from "react";
-
 import AllUsersTable from "@/components/admin/allUsersTable/AllUsersTable";
 import PageContentSection from "@/components/admin/pageContentSection/PageContentSection";
 
 export default function AllUsersPage() {
   return (
     <PageContentSection>
-      <Suspense fallback="Loading">
-        <AllUsersTable />
-      </Suspense>
+      <AllUsersTable />
     </PageContentSection>
   );
 }

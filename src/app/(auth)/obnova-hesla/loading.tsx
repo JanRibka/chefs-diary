4dd20ae5dfaacd66ff65a025780d53160002b4f8
JanRibka <62721322+JanRibka@users.noreply.github.com>
2025-06-05 23:53:27 +0200

    Change spinner from export to component

--- src/app/(auth)/obnova-hesla/loading.tsx
+++ src/app/(auth)/obnova-hesla/loading.tsx
@@ -4,17 +4,7 @@ export default function Loading() {
   return (
     <section>
       <div className="w-full">
-        <div className="w-full flex content-center justify-center">
-          <Spinner
-            aria-label="Loading..."
-            color="primary"
-            classNames={{
-              circle1: "w-28 h-28",
-              circle2: "w-28 h-28",
-              wrapper: "w-28 h-28",
-            }}
-          />
-        </div>
+        <Spinner />
       </div>
     </section>
   );
--- src/components/shared/spinner/Spinner.tsx
+++ src/components/shared/spinner/Spinner.tsx
@@ -1,5 +1,28 @@
 "use client";
 
-import { Spinner } from "@heroui/react";
+import { Spinner as SpinnerHero, SpinnerProps } from "@heroui/react";
 
-export default Spinner;
+type Props = SpinnerProps & {};
+
+export default function Spinner({
+  "aria-label": ariaLabel,
+  color,
+  classNames,
+  ...restProps
+}: Props) {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <SpinnerHero
+        aria-label={ariaLabel ?? "Načítání..."}
+        color={color ?? "primary"}
+        classNames={{
+          circle1: "w-28 h-28",
+          circle2: "w-28 h-28",
+          wrapper: "w-28 h-28",
+          ...classNames,
+        }}
+        {...restProps}
+      />
+    </div>
+  );
+}

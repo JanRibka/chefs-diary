2e8503cdd7351ed2d93b5981de935c9f76034f97
JanRibka <62721322+JanRibka@users.noreply.github.com>
2025-07-05 01:12:04 +0200

    [API] =Uprava funkce getUnitGroupsWithAssignedUnits (#9)
    * Added circleci config
    
    * Add discussion templates
    
    * Correct directory name workflows
    
    * Zakomentováni Spuštění testů v ci.yml
    
    * OPrava syntax error v closed-issue-message.yml
    
    * Přidán build skript do package.json
    
    * Přidání prisma generate do ci.yml
    
    * Uprava modelu UnitGroupsWithAssignmentsDTO
    
    * =Uprva funkce getUnitGroupsWithAssignedUnits

--- src/lib/repositories/unitRepository.ts
+++ src/lib/repositories/unitRepository.ts
@@ -190,7 +190,7 @@ export async function deleteUnitGroup(idUnitGroup: number) {
 export async function getUnitGroupsWithAssignedUnits(
   idUnit: number
 ): Promise<UnitGroupsWithAssignmentsDTO[]> {
-  return await prisma.unitGroup.findMany({
+  const result = await prisma.unitGroup.findMany({
     relationLoadStrategy: "join",
     select: {
       idUnitGroup: true,
@@ -217,6 +217,8 @@ export async function getUnitGroupsWithAssignedUnits(
       },
     },
   });
+
+  return result satisfies UnitGroupsWithAssignmentsDTO[];
 }
 
 /**

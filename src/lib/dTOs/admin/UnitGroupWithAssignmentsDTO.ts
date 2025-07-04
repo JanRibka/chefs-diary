729d9b273d0dc4142455645802a7b751c134fde7
JanRibka <62721322+JanRibka@users.noreply.github.com>
2025-07-05 01:22:16 +0200

    [API] =Uprava modelu UnitGroupsWithAssignmentsDTO (#10)
    * Added circleci config
    
    * Add discussion templates
    
    * Correct directory name workflows
    
    * Zakomentováni Spuštění testů v ci.yml
    
    * OPrava syntax error v closed-issue-message.yml
    
    * Přidán build skript do package.json
    
    * Přidání prisma generate do ci.yml
    
    * Uprava modelu UnitGroupsWithAssignmentsDTO
    
    * =Uprva funkce getUnitGroupsWithAssignedUnits
    
    * Úprava modelu UnitGroupsWithAssignmentsDTO

--- src/lib/dTOs/admin/UnitGroupWithAssignmentsDTO.ts
+++ src/lib/dTOs/admin/UnitGroupWithAssignmentsDTO.ts
@@ -1,11 +1,20 @@
-import { Unit, UnitGroup, UnitGroupUnit } from "@prisma/client";
+// export type UnitGroupsWithAssignmentsDTO = Pick<
+//   UnitGroup,
+//   "idUnitGroup" | "name"
+// > & {
+//   baseUnit: Pick<Unit, "idUnit" | "name"> | null;
+//   unitGroupUnit: (Pick<UnitGroupUnit, "isBaseUnit" | "idUnit"> & {
+//     unit: Pick<Unit, "name">;
+//   })[];
+// };
 
-export type UnitGroupsWithAssignmentsDTO = Pick<
-  UnitGroup,
-  "idUnitGroup" | "name"
-> & {
-  baseUnit: Pick<Unit, "idUnit" | "name"> | null;
-  unitGroupUnit: (Pick<UnitGroupUnit, "isBaseUnit" | "idUnit"> & {
-    unit: Pick<Unit, "name">;
-  })[];
-};
+export type UnitGroupsWithAssignmentsDTO = {
+  unitGroupUnit: {
+    unit: { name: string };
+    idUnit: number;
+    isBaseUnit: boolean;
+  }[];
+  name: string;
+  idUnitGroup: number;
+  baseUnit: { name: string; idUnit: number } | null;
+}[];

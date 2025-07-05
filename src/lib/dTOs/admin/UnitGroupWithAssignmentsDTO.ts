c629ab100d78521ac96c54a9f3396f7760555dd1
JanRibka <62721322+JanRibka@users.noreply.github.com>
2025-07-05 15:58:23 +0200

    Git/build (#16)
    * Added circleci config
    
    * Add discussion templates
    
    * Correct directory name workflows
    
    * Zakomentováni Spuštění testů v ci.yml
    
    * OPrava syntax error v closed-issue-message.yml
    
    * Přidán build skript do package.json
    
    * Přidání prisma generate do ci.yml
    
    * Uprava modelu UnitGroupsWithAssignmentsDTO
    
    * =Uprva funkce getUnitGroupsWithAssignedUnits
    
    * Úprava modelu UnitGroupsWithAssignmentsDTO
    
    * Dalsi upravy
    
    * Test
    
    * Posledni test dneska
    
    * Uprava ci.yml
    
    * Uprava ci.yml
    
    * Uprava  UnitGroupsWithAssignmentsDTO

--- src/lib/dTOs/admin/UnitGroupWithAssignmentsDTO.ts
+++ src/lib/dTOs/admin/UnitGroupWithAssignmentsDTO.ts
@@ -8,14 +8,39 @@
 //   })[];
 // };
 
-export type UnitGroupsWithAssignmentsDTO = {
-  unitGroupUnit: {
-    unit: { name: string };
-    idUnit: number;
-    isBaseUnit: boolean;
-  }[];
-  name: string;
-  idUnitGroup: number;
-  baseUnit: { name: string; idUnit: number } | null;
-};
+import { Prisma } from "@prisma/client";
 
+// export type UnitGroupsWithAssignmentsDTO = {
+//   unitGroupUnit: {
+//     unit: { name: string };
+//     idUnit: number;
+//     isBaseUnit: boolean;
+//   }[];
+//   name: string;
+//   idUnitGroup: number;
+//   baseUnit: { name: string; idUnit: number } | null;
+// };
+
+export type UnitGroupsWithAssignmentsDTO = Prisma.UnitGroupGetPayload<{
+  select: {
+    idUnitGroup: true;
+    name: true;
+    baseUnit: {
+      select: {
+        idUnit: true;
+        name: true;
+      };
+    };
+    unitGroupUnit: {
+      select: {
+        isBaseUnit: true;
+        idUnit: true;
+        unit: {
+          select: {
+            name: true;
+          };
+        };
+      };
+    };
+  };
+}>;

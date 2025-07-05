cf0859adb7f349abec553b8c1c4b311d09b8db66
JanRibka <62721322+JanRibka@users.noreply.github.com>
2025-07-05 16:32:39 +0200

    Git/build (#19)
    * Added circleci config
    
    * Add discussion templates
    
    * Correct directory name workflows
    
    * Zakomentováni Spuštění testů v ci.yml
    
    * OPrava syntax error v closed-issue-message.yml
    
    * Přidán build skript do package.json
    
    * Přidání prisma generate do ci.yml
    
    * Uprava modelu UnitGroupsWithAssignmentsDTO
    
    * =Uprva funkce getUnitGroupsWithAssignedUnits
    
    * Úprava modelu UnitGroupsWithAssignmentsDTO
    
    * Dalsi upravy
    
    * Test
    
    * Posledni test dneska
    
    * Uprava ci.yml
    
    * Uprava ci.yml
    
    * Uprava  UnitGroupsWithAssignmentsDTO
    
    * Uprava getUnitGroupsWithAssignedUnits
    
    * Uprava units repository
    
    * Update prisma

--- src/lib/dTOs/admin/UnitGroupWithAssignmentsDTO.ts
+++ src/lib/dTOs/admin/UnitGroupWithAssignmentsDTO.ts
@@ -1,46 +1,11 @@
-// export type UnitGroupsWithAssignmentsDTO = Pick<
-//   UnitGroup,
-//   "idUnitGroup" | "name"
-// > & {
-//   baseUnit: Pick<Unit, "idUnit" | "name"> | null;
-//   unitGroupUnit: (Pick<UnitGroupUnit, "isBaseUnit" | "idUnit"> & {
-//     unit: Pick<Unit, "name">;
-//   })[];
-// };
+import { Unit, UnitGroup, UnitGroupUnit } from "@prisma/client";
 
-import { Prisma } from "@prisma/client";
-
-// export type UnitGroupsWithAssignmentsDTO = {
-//   unitGroupUnit: {
-//     unit: { name: string };
-//     idUnit: number;
-//     isBaseUnit: boolean;
-//   }[];
-//   name: string;
-//   idUnitGroup: number;
-//   baseUnit: { name: string; idUnit: number } | null;
-// };
-
-export type UnitGroupsWithAssignmentsDTO = Prisma.UnitGroupGetPayload<{
-  select: {
-    idUnitGroup: true;
-    name: true;
-    baseUnit: {
-      select: {
-        idUnit: true;
-        name: true;
-      };
-    };
-    unitGroupUnit: {
-      select: {
-        isBaseUnit: true;
-        idUnit: true;
-        unit: {
-          select: {
-            name: true;
-          };
-        };
-      };
-    };
-  };
-}>;
+export type UnitGroupsWithAssignmentsDTO = Pick<
+  UnitGroup,
+  "idUnitGroup" | "name"
+> & {
+  baseUnit: Pick<Unit, "idUnit" | "name"> | null;
+  unitGroupUnit: (Pick<UnitGroupUnit, "isBaseUnit" | "idUnit"> & {
+    unit: Pick<Unit, "name">;
+  })[];
+};

fdb1b4250b3570018a4fa35351a518bfba84c297
JanRibka <ribkaj@seznam.cz>
2025-11-14 23:59:59 +0100

    Přihlášení

--- src/app/(auth)/overeni-emailu/[token]/page.tsx
+++ src/app/(auth)/overeni-emailu/[token]/page.tsx
@@ -1,13 +1,13 @@
-import FormAlert from "@/components/shared/form/FormAlert";
-import PasswordResetAlert from "@/components/shared/passwordResetAlert/PasswordResetAlert";
-import VerifyEmailStatusEnum from "@/lib/enums/VerifyEmailStatusEnum";
-import getErrorTextByKey from "@/lib/errorLibrary/auth/authErrorLibrary";
-import { getVerificationTokenByToken } from "@/lib/repositories/verificationTokenRepository";
-import webRoutes from "@/lib/routes/webRoutes";
-import { verifyEmail } from "@/lib/services/verifyEmailService";
+import EmailNotVerified from '@/components/shared/auth/logIn/EmailNotVerified/EmailNotVerified';
+import FormAlert from '@/components/shared/form/FormAlert';
+import PasswordResetAlert from '@/components/shared/passwordResetAlert/PasswordResetAlert';
+import VerifyEmailStatusEnum from '@/lib/enums/VerifyEmailStatusEnum';
+import getErrorTextByKey from '@/lib/errorLibrary/auth/authErrorLibrary';
+import { getVerificationTokenByToken } from '@/lib/repositories/verificationTokenRepository';
+import webRoutes from '@/lib/routes/webRoutes';
+import { verifyEmail } from '@/lib/services/verifyEmailService';
 
-import EmailNotVerified from "@/components/shared/auth/logIn/EmailNotVerified/EmailNotVerified";
-import VerifyEmailSuccessful from "../../../../components/shared/auth/verifyEmailSuccess/VerifyEmailSuccess";
+import VerifyEmailSuccessful from '../../../../components/shared/auth/verifyEmailSuccess/VerifyEmailSuccess';
 
 type Props = {
   params: Promise<{ token: string }>;
